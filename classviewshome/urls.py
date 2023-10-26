81f183463591e77c47e4dda79c41d44e58ea96c6
MKA <MKA@ULTOP.local>
2023-10-26 21:07:47 +0400

    Изучаем классы джанго

--- classviewshome/urls.py
+++ classviewshome/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView,BlogView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('blog/', BlogView.as_view(), name='blog')
 ]
--- classviewshome/views.py
+++ classviewshome/views.py
@@ -1,7 +1,33 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Article
+
 
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'classviewshome/home.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['custom_date'] = 'Доп информация'
+        return context
+
+
+class BlogView(ListView):
+    model = Article
+
+    def head(self, *args, **kwargs):
+        last_book = self.get_queryset().latest("publication_date")
+        response = HttpResponse(
+            # RFC 1123 date format.
+            headers={
+                "Last-Modified": last_book.publication_date.strftime(
+                    "%a, %d %b %Y %H:%M:%S GMT"
+                )
+            },
+        )
+        return response
+
+
+
+
