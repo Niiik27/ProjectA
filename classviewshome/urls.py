b943a8ce4cfcb1b7fd0cdb2ea126f7832e6f8ed0
MKA <MKA@ULTOP.local>
2023-10-31 21:25:04 +0400

    Сделали добавление, редактирование, удаление

--- classviewshome/urls.py
+++ classviewshome/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import HomeView,BlogView
+from .views import HomeTempateView, ArticleListView, ArticleDetailView, ArticleCreateView, BookCreateView, \
+    ArticleUpdateView, ArticleDeleteView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('blog/', BlogView.as_view(), name='blog')
+    path('', HomeTempateView.as_view(), name='home'),
+    path('articles/', ArticleListView.as_view(), name='article_list'),
+    path('articles/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+    path('update_article/<int:pk>/', ArticleUpdateView.as_view(), name='update_article'),
+    path('delete_article/<int:pk>/', ArticleDeleteView.as_view(), name='delete_article'),
+    path('create_article/', ArticleCreateView.as_view(), name='create_article'),
+    path('create_book/', BookCreateView.as_view(), name='create_book'),
 ]
--- classviewshome/views.py
+++ classviewshome/views.py
@@ -1,19 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
-from .models import Article
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .forms import ArticleForm, BookForm
+from .models import Article, Book
 
 
-class HomeView(TemplateView):
+class HomeTempateView(TemplateView):
     template_name = 'classviewshome/home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['custom_date'] = 'Доп информация'
+        context['custom_date'] = 'Напиши в адресной строке blog'
         return context
 
 
-class BlogView(ListView):
+class ArticleListView(ListView):
     model = Article
 
     def head(self, *args, **kwargs):
@@ -29,5 +31,47 @@ class BlogView(ListView):
         return response
 
 
+class ArticleDetailView(DetailView):
+    model = Article
+    # context_object_name = "q"
+    # template_name = 'classviewshome/home.html'
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data()
+        context['article_books']=Book.objects.filter(article = self.object)
+        return context
 
 
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleForm
+    success_url = '/articles/'
+
+    # context_object_name = "q"
+    # template_name = 'classviewshome/home.html'
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data()
+    #     context['article_books']=Book.objects.filter(article = self.object)
+    #     return context
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    # form_class = ArticleForm
+    success_url = '/articles/'
+    # context_object_name = "q"
+    # template_name = 'classviewshome/home.html'
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data()
+    #     context['article_books']=Book.objects.filter(article = self.object)
+    #     return context
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleForm
+    success_url = '/articles/'
+
+
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+    success_url = '/articles/'
